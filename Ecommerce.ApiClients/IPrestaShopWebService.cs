272fc39eb3b6a9bd3fb6aac381b1daa703a87832
ktamero <kris@amero.dk>
2015-01-07 09:56:14 +0100

    Forgot to add file

---
+++ Ecommerce.ApiClients/IPrestaShopWebService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Ecommerce.Prestashop
+{
+    public interface IPrestaShopWebService
+    {
+        Version CurrentVersion { get; }
+
+        async Task<XElement> Add(string resource, XElement xml);
+        async Task<XElement> AddWithUrl(string url, XElement xml);
+
+        async Task<XElement> Get(string resource);
+        async Task<XElement> Get(string resource, int id);
+        async Task<XElement> Get(string resource, Dictionary<string, string> options);
+        async Task<XElement> Get(string resource, int? id, Dictionary<string, string> options);
+        async Task<XElement> GetWithUrl(string url);
+
+        async Task<string> Head(string resource);
+        async Task<string> Head(string resource, int id);
+        async Task<string> Head(string resource, int? id, Dictionary<string, string> options);
+        async Task<string> HeadWithUrl(string url);
+
+        async Task<XElement> Edit(string resource, int id, XElement xml);
+        async Task<XElement> EditWithUrl(string url, XElement xml);
+
+        async Task Delete(string resource, int id);
+        async Task Delete(string resource, int[] ids);
+        async Task DeleteWithUrl(string url);
+    }
+}
