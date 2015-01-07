99d250dfc7e7a7023654f48b2924e4afed4c0681
ktamero <kris@amero.dk>
2015-01-07 09:57:34 +0100

    async is not allowed in interfaces , my mistake ;)

--- Ecommerce.ApiClients/IPrestaShopWebService.cs
+++ Ecommerce.ApiClients/IPrestaShopWebService.cs
@@ -9,25 +9,25 @@ namespace Ecommerce.Prestashop
     {
         Version CurrentVersion { get; }
 
-        async Task<XElement> Add(string resource, XElement xml);
-        async Task<XElement> AddWithUrl(string url, XElement xml);
+        Task<XElement> Add(string resource, XElement xml);
+        Task<XElement> AddWithUrl(string url, XElement xml);
 
-        async Task<XElement> Get(string resource);
-        async Task<XElement> Get(string resource, int id);
-        async Task<XElement> Get(string resource, Dictionary<string, string> options);
-        async Task<XElement> Get(string resource, int? id, Dictionary<string, string> options);
-        async Task<XElement> GetWithUrl(string url);
+        Task<XElement> Get(string resource);
+        Task<XElement> Get(string resource, int id);
+        Task<XElement> Get(string resource, Dictionary<string, string> options);
+        Task<XElement> Get(string resource, int? id, Dictionary<string, string> options);
+        Task<XElement> GetWithUrl(string url);
 
-        async Task<string> Head(string resource);
-        async Task<string> Head(string resource, int id);
-        async Task<string> Head(string resource, int? id, Dictionary<string, string> options);
-        async Task<string> HeadWithUrl(string url);
+        Task<string> Head(string resource);
+        Task<string> Head(string resource, int id);
+        Task<string> Head(string resource, int? id, Dictionary<string, string> options);
+        Task<string> HeadWithUrl(string url);
 
-        async Task<XElement> Edit(string resource, int id, XElement xml);
-        async Task<XElement> EditWithUrl(string url, XElement xml);
+        Task<XElement> Edit(string resource, int id, XElement xml);
+        Task<XElement> EditWithUrl(string url, XElement xml);
 
-        async Task Delete(string resource, int id);
-        async Task Delete(string resource, int[] ids);
-        async Task DeleteWithUrl(string url);
+        Task Delete(string resource, int id);
+        Task Delete(string resource, int[] ids);
+        Task DeleteWithUrl(string url);
     }
 }
